a94cda7b75e2db4efae971cbd834e51ea91cfe68
Ezreal Smith <598280017@qq.com>
2021-02-26 17:28:21 +0800

    Update HttppDispatcher.java

--- example/src/main/java/io/netty/example/web/HttppDispatcher.java
+++ example/src/main/java/io/netty/example/web/HttppDispatcher.java
@@ -7,7 +7,8 @@ public class HttpDispatcher HttpChannelHandlable {
         dispatchRequest(ctx, request);
     }
   public void dispatchRequest(ChannelHandlerContext ctx, FullHttpRequest request) {
-        if (HttpUtil.is100ContinueExpected(request)) {
+      // netty 源码中有对100的返回, 1.0协议  
+      if (HttpUtil.is100ContinueExpected(request)) {
             ctx.write(new DefaultFullHttpResponse(HTTP_1_1, CONTINUE, Unpooled.EMPTY_BUFFER));
             LOG.error("ommitted and continue since is100ContinueExpected request:{}", request);
             return;
@@ -18,40 +19,42 @@ public class HttpDispatcher HttpChannelHandlable {
             return;
         }
 
-        // parse url path and router from routersMap, delivery request
+        // 解析路由转发
         Map<HttpLabel, HttpHandlable> map = DefaultHttpRouters.getInstance().getRoutersMap();
-        String uri = request.uri();
+        String httpuri = request.uri();
         HttpMethod method = request.method();
-        if (uri.contains("?")) {
-            uri = uri.substring(0, uri.indexOf("?"));
+        if (httpuri.contains("?")) {
+            httpuri = httpuri.substring(0, httpuri.indexOf("?"));
         }
 
-        FullHttpResponse response = new DefaultFullHttpResponse(request.protocolVersion(), OK);
-        response.headers().set(CONTENT_TYPE, TEXT_PLAIN);
+        FullHttpResponse res = new DefaultFullHttpResponse(request.protocolVersion(), OK);
+        res.headers().set(CONTENT_TYPE, TEXT_PLAIN);
         HttpHandlable handler = map.get(new HttpLabel(uri, method));
 
         /**
-         * method not found, handler not exists in
+         * handler not exists inrouters
          **/
         if (handler == null) {
-            response.setStatus(NOT_FOUND).headers().setInt(CONTENT_LENGTH, response.content().readableBytes());
-            ctx.write(response).addListener(ChannelFutureListener.CLOSE);
-            LOG.error("failed to handle method:{}, uri:{}, handler is null", method, uri);
+            response.setStatus(NOT_FOUND).headers().setInt(CONTENT_LENGTH, respo.content().readableBytes());
+            ctx.write(res).addListener(ChannelFutureListener.CLOSE);
+            LOG.error("failed to handle method:{}, uri:{}, handler is null", httpMethod, httpuri);
             return;
         }
 
-        // handle request support: http2, http, websocket
+        // handle: http2, http, websocket
         handler.handle(ctx, request, response);
+        // netty 会解析头部是否为默认
         boolean keepAlive = HttpUtil.isKeepAlive(request);
-        LOG.info("succeed to handle method:{}, uri:{}", method, uri);
+        LOG.info("succeed to handle method:{}, uri:{}", httpMethod, httpuri);
 
-        // keep alive for keepAlive request, also for http1.0 since 1.0 default value is close
-        response.headers().set(CONNECTION, CLOSE);
+        // 是否对100, 保留心跳,netty源码是默认支持
+        // keep alive for request with keepAlive, also http1.0 since 1.0 default value is close
+        res.headers().set(CONNECTION, CLOSE);
         if (keepAlive || (isKeepAlive(request) && request.protocolVersion().equals(HTTP_1_0))) {
-            response.headers().set(CONNECTION, KEEP_ALIVE);
+            res.headers().set(CONNECTION, KEEP_ALIVE);
         }
 
-        ChannelFuture future = ctx.write(response);
-        future.addListener(ChannelFutureListener.CLOSE_ON_FAILURE);
+        ChannelFuture f = ctx.write(response);
+        f.addListener(ChannelFutureListener.CLOSE_ON_FAILURE);
     }
 }
